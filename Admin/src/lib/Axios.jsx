62d0e81c942b3f3c0201f86b3b84313986a74a29
[ali edris] <[almusdarthafa999@gmail.com]>
2024-12-08 04:09:42 -0800

    admin

--- Admin/src/lib/Axios.jsx
+++ Admin/src/lib/Axios.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = import.meta.env.VITE_API_URL;
+const url = "https://myresturant-1.onrender.com";
 export const axiosInstance = axios.create({
   baseURL: `${url}/api/v1`,
   withCredentials: true,
