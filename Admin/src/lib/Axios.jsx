a8c44d620a9406bb6a2b85f7fea84d3114404c6a
[ali edris] <[almusdarthafa999@gmail.com]>
2024-12-10 06:04:24 -0800

    b14

--- Admin/src/lib/Axios.jsx
+++ Admin/src/lib/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const url = "https://myresturant-1.onrender.com";
-const url = "http://localhost:4000";
+const url = "https://myresturant-1.onrender.com";
+// const url = "http://localhost:4000";
 export const axiosInstance = axios.create({
   baseURL: `${url}/api/v1`,
   withCredentials: true,
--- frontend/src/lib/Axios.jsx
+++ frontend/src/lib/Axios.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const url = "http://localhost:4000";
+// const url = "http://localhost:4000";
+const url = "https://myresturant-1.onrender.com";
 export const axiosInstance = axios.create({
   baseURL: `${url}/api/v1`,
   withCredentials: true,
